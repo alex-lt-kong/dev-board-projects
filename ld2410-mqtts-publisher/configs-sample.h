1a51f95546ddd56ce8d1246a7290ed7eed307972
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-11-13 20:45:15 +0800

    new sample configs

--- ld2410-mqtts-publisher/configs-sample.h
+++ ld2410-mqtts-publisher/configs-sample.h
@@ -4,7 +4,7 @@
 #define MQTT_CLIENT_ID "MyMqttClient"
 #define MQTT_SERVER "mqtt.example.org"
 #define MQTT_PORT 1883
-#define MQTT_TOPIC "/mqtt/topic"
+#define MQTT_TOPIC "mqtt/topic"
 #define MQTT_USERNAME "user"
 #define MQTT_PASSWORD "pa$$w0rd"
 #define MQTT_POSITIVE_PUBLISH_INTERVAL_MS 1000
